cd681a5f162c19b4532d21921e4b6c845fc90905
Ranjitrajak <mail4ranjitrajak@gmail.com>
2022-05-04 13:26:10 +0530

    new

---
+++ src/models/CollegeModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+//const ObjectId = mongoose.Schema.Types.ObjectId
+//{ name: { mandatory, unique, example iith}, fullName: {mandatory, example `Indian Institute of Technology, Hyderabad`}, logoLink: {mandatory}, isDeleted: {boolean, default: false} }
+
+const CollegeSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true
+
+    },
+    logoLink:{
+        type:URL,
+        required:true,
+
+
+    },
+    isDeleted: {
+        type:Boolean,
+        default:false
+    }
+    
+
+
+   
+
+}, { timestamps: true });
+
+
+
+
+    module.exports = mongoose.model('College',CollegeSchema )
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const authorController = require("../controllers/authorController")
-const blogController = require("../controllers/blogController")
 const Middleware=require("../middleware/auth")
 
 
-router.post("/createAuthor", authorController.createAuthor)
 
-router.post("/createBlog",Middleware.authentication,Middleware.authorization, blogController.createBlog)
 
-router.put("/blogs/:blogId", Middleware.authentication,Middleware.authorization,blogController.BlogById)
-
-router.get("/getBlogList",Middleware.authentication, blogController.Bloglist)
-
-router.delete("/blogs", Middleware.authentication,Middleware.authorization,blogController.deleteData)
-
-router.delete("/blog/:blogId",Middleware.authentication,Middleware.authorization,blogController.deleteBlogData)
-
-router.post("/login", authorController.authorLogIn)
 
 module.exports = router;
 
